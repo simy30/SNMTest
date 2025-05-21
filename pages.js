730de71d0a1dc2641407e42aacfc4b23a9859bb8
simy30 <121553442+simy30@users.noreply.github.com>
2025-05-22 04:10:14 +0530

    Update pages.js

--- pages.js
+++ pages.js
@@ -5,10 +5,11 @@ export default async (req, res) => {
   const response = await fetch('https://api.webflow.com/v2/sites/62b33d4be1f1d95580bb1e35/pages?limit=200&offset=0', {
     method: 'GET',
     headers: {
-      'Authorization': 'Bearer YOUR_SECRET_API_KEY',
+      'Authorization': '6674f5c7dfffe80afaff9453d62b74adde6b6772661fb228f8812ca8345c7bd9',
       'accept-version': '2.0.0'
     }
   });
   const data = await response.json();
   res.status(200).json(data);
 };
+
